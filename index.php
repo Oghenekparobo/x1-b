8fd0ad6986250d66eec5fc3586544708ebae5712
STEPHEN ONOKWAKPOR <oghenekparobo>
2023-09-08 10:03:46 +0100

    added the json format

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: application/json'); 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
 $currentUtcTime = new \DateTime('now', new \DateTimeZone('UTC'));
@@ -6,11 +7,14 @@ $currentUtcTimeStamp = $currentUtcTime->format('Y-m-d\TH:i:s\Z');
 
 $currentDayOfWeek = date("l");
 
+$name = $_GET['slack_name'];
+$track = $_GET['track'];
+
 $data = array(
-    "slack_name" => "pablo",
+    "slack_name" => $name,
     "current_day" => $currentDayOfWeek,
     "utc_time" => $currentUtcTimeStamp,
-    "track" => "backend",
+    "track" => $track,
     "github_file_url" => "https://github.com/Oghenekparobo/x1-b/blob/main/index.php",
     "github_repo_url" => "https://github.com/Oghenekparobo/x1-b",
     "status_code" => 200
